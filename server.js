8f4bbb541693f786e73cad57e4e1cf0e9ae65c1a
Zayidu Ansari <zayidu11@gmail.com>
2020-05-25 12:52:56 +0400

    Commit to 2. Express & MongoDB Setup

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get("/", (req, res) => res.send("API Running!"));
+// Connect Database
+connectDB();
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api//posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
+app.get('/', (req, res) => res.send('API Running!'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server Started ON ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started on ${PORT}`));
