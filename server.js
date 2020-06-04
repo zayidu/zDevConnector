a31c13ae19fd853537ac480238748b36341bdd7a
Zayidu Ansari <zayidu11@gmail.com>
2020-06-05 01:01:40 +0400

    Prepare for Deployment - Zayidu A

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -19,7 +20,15 @@ app.use('/api/posts', require('./routes/api//posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/profiles', require('./routes/api/profile'));
 
-app.get('/', (req, res) => res.send('API Running!'));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
